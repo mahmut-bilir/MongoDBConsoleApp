f7d7fd74ba38e5fc151a23267e108e920aa327e1
Mahmut Bilir <mahmut-bilir@hotmail.com>
2024-11-08 23:15:13 +0300

    mongodb code  added to program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+var client = new MongoClient("mongodb://localhost:27017");
+
+var database = client.GetDatabase("testdb");
+
+var collection = database.GetCollection<BsonDocument>("persons");
+
+var person = new BsonDocument
+{
+    { "name", "Mahmut" },
+    { "age", 30 },
+    { "profession", "Software Developer" }
+};
+
+await collection.InsertOneAsync(person);
+Console.WriteLine("Data Inserted!");
+
+var filter = Builders<BsonDocument>.Filter.Eq("name", "Mahmut");
+var result = await collection.Find(filter).ToListAsync();
+
+foreach (var doc in result)
+{
+    Console.WriteLine(doc);
+}
